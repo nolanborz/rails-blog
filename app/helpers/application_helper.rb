ddd5b0875517001ebe1ef3417f3bc140b80e6775
nolanborz <nolanrborzoni@gmail.com>
2025-04-04 12:49:36 +0900

    Stylize the blog a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def markdown(text)
+    return "" if text.blank?
     renderer = Redcarpet::Render::HTML.new(hard_wrap: true, safe_links_only: true)
     markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true, fenced_code_blocks: true)
     sanitize markdown.render(text)
